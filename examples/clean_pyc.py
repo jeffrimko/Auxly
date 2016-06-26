6d56a1bde11788480c75c45e97d2f3aa818b6fcd
unknown <jeffrimko@gmail.com>
2016-06-26 16:22:59 -0400

    Added first example.

---
+++ examples/clean_pyc.py
@@ -0,0 +1,14 @@
+import auxly
+import qprompt
+delfunc = lambda is_test: auxly.filesys.delete("..", "\.pyc$", recurse=True, test=is_test)
+delfiles = delfunc(True)
+if len(delfiles):
+    print("Files to delete:")
+    print("\n".join(["    " + i for i in delfiles]))
+    if qprompt.ask_yesno("OK to delete?"):
+        if delfunc(False) == delfiles:
+            qprompt.alert("Files deleted.")
+        else:
+            qprompt.warn("Some files not deleted!")
+else:
+    qprompt.alert("No files to delete.")
