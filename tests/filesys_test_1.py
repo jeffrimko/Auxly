785a77eff07b92afd8c1d01f5984b69e631e576a
unknown <jeffrimko@gmail.com>
2016-12-24 11:34:41 -0500

    Updated tests to check more behavior. Minor style updates.

--- tests/filesys_test_1.py
+++ tests/filesys_test_1.py
@@ -2,7 +2,7 @@
 ## SECTION: Imports                                             #
 ##==============================================================#
 
-import os
+import os.path as op
 from testlib import *
 
 from auxly.filesys import makedirs, delete, isempty, copy
@@ -15,28 +15,28 @@ class TestCase(unittest.TestCase):
 
     def test_makedirs_1(test):
         path = "foo/bar"
-        test.assertFalse(os.path.exists(path))
+        test.assertFalse(op.exists(path))
         test.assertTrue(makedirs(path))
-        test.assertTrue(os.path.exists(path))
-        test.assertTrue(os.path.isdir(path))
+        test.assertTrue(op.exists(path))
+        test.assertTrue(op.isdir(path))
         test.assertTrue(isempty(path))
         test.assertTrue(delete(path))
-        test.assertFalse(os.path.exists(path))
+        test.assertFalse(op.exists(path))
         test.assertTrue(delete(path.split("/")[0]))
 
     def test_makedirs_2(test):
         path1 = "../foo"
         path2 = "../bar"
-        test.assertFalse(os.path.exists(path1))
+        test.assertFalse(op.exists(path1))
         test.assertTrue(makedirs(path1))
-        test.assertTrue(os.path.exists(path1))
-        test.assertTrue(os.path.isdir(path1))
+        test.assertTrue(op.exists(path1))
+        test.assertTrue(op.isdir(path1))
         test.assertTrue(isempty(path1))
         test.assertTrue(copy(path1, path2))
         test.assertTrue(delete(path1))
         test.assertTrue(delete(path2))
-        test.assertFalse(os.path.exists(path1))
-        test.assertFalse(os.path.exists(path2))
+        test.assertFalse(op.exists(path1))
+        test.assertFalse(op.exists(path2))
 
 ##==============================================================#
 ## SECTION: Main Body                                           #
--- tests/filesys_test_2.py
+++ tests/filesys_test_2.py
@@ -2,10 +2,18 @@
 ## SECTION: Imports                                             #
 ##==============================================================#
 
-import os
+import os.path as op
 from testlib import *
 
-from auxly.filesys import copy, isempty, delete
+from auxly.filesys import makedirs, copy, isempty, delete
+
+##==============================================================#
+## SECTION: Global Definitions                                  #
+##==============================================================#
+
+FNAME = ["foo.txt", "bar.txt"]
+DIR = ["foo", "bar"]
+TEXT = ["hello", "world"]
 
 ##==============================================================#
 ## SECTION: Class Definitions                                   #
@@ -13,18 +21,72 @@ from auxly.filesys import copy, isempty, delete
 
 class TestCase(unittest.TestCase):
 
+    def tearDown(test):
+        for i in FNAME + DIR:
+            delete(i)
+
     def test_copy_1(test):
-        path = "foo.txt"
+        path = FNAME[0]
         test.assertIsNone(isempty(path))
         with open(path, "w") as fo:
             pass
         test.assertTrue(isempty(path))
         with open(path, "w") as fo:
-            fo.write("not empty")
+            fo.write(TEXT[0])
         test.assertFalse(isempty(path))
         test.assertTrue(delete(path))
         test.assertIsNone(isempty(path))
         test.assertFalse(delete(path))
+
+    def test_copy_2(test):
+        """Copy file to dir/file, overwrite existing file."""
+        path0 = FNAME[0]
+        path1 = op.join(DIR[0], FNAME[0])
+        fwrite(path0, TEXT[0])
+        fwrite(path1, TEXT[1])
+        test.assertTrue(TEXT[1] == fread(path1))
+        test.assertTrue(copy(path0, path1))
+        test.assertTrue(TEXT[0] == fread(path1))
+
+    def test_copy_3(test):
+        """Copy file to dir/file fails due to overwrite flag."""
+        path0 = FNAME[0]
+        path1 = op.join(DIR[0], FNAME[0])
+        fwrite(path0, TEXT[0])
+        fwrite(path1, TEXT[1])
+        test.assertTrue(TEXT[1] == fread(path1))
+        test.assertFalse(copy(path0, path1, overwrite=False))
+        test.assertTrue(TEXT[1] == fread(path1))
+
+    def test_copy_4(test):
+        """Copy file to dir."""
+        path0 = FNAME[0]
+        fwrite(path0, TEXT[0])
+        path1 = DIR[1]
+        test.assertTrue(copy(path0, path1))
+        path1 = op.join(path1, path0)
+        test.assertTrue(TEXT[0] == fread(path1))
+
+    def test_copy_5(test):
+        """Copy file to dir fails due to overwrite flag."""
+        # import qprompt
+        # qprompt.pause()
+        path0 = FNAME[0]
+        path1 = DIR[0]
+        fwrite(path0, TEXT[0])
+        fpath1 = op.join(path1, path0)
+        fwrite(fpath1, TEXT[1])
+        test.assertFalse(copy(path0, path1, overwrite=False))
+        test.assertTrue(TEXT[1] == fread(fpath1))
+
+def fwrite(path, text):
+    makedirs(path)
+    with open(path, "w") as fo:
+        fo.write(text)
+
+def fread(path):
+    with open(path) as fi:
+        return fi.read()
 
 ##==============================================================#
 ## SECTION: Main Body                                           #
