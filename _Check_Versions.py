59fe997405db9fd5ece91168e90a91dde3c1c216
unknown <jeffrimko@gmail.com>
2016-12-24 11:35:46 -0500

    Bumped alpha version number.

--- _Check_Versions.py
+++ _Check_Versions.py
@@ -12,7 +12,7 @@ import verace
 VERCHK = verace.VerChecker("Auxly", __file__)
 VERCHK.include(r"lib\setup.py", match="version = ", delim='"')
 VERCHK.include(r"lib\auxly\_modu.py", match="__version__ = ", delim='"')
-# VERCHK.include(r"CHANGELOG.adoc", match="qprompt-", delim="-", delim2=" ", updatable=False)
+VERCHK.include(r"CHANGELOG.adoc", match="auxly-", delim="-", delim2=" ", updatable=False)
 
 ##==============================================================#
 ## SECTION: Main Body                                           #
--- lib/setup.py
+++ lib/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "auxly",
-    version = "0.1.0",
+    version = "0.2.0-alpha1",
     author = "Jeff Rimko",
     author_email = "jeffrimko@gmail.com",
     description = "Python library for common shell-like script tasks.",
