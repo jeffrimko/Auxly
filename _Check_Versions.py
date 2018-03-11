59bf4c8bbfd055347d57386345610b96ba71a999
jeffrimko <jeffrimko@gmail.com>
2018-03-11 19:48:17 -0400

    Minor doc changes.

--- _Check_Versions.py
+++ _Check_Versions.py
@@ -12,6 +12,8 @@ VERCHK = verace.VerChecker("Auxly", __file__)
 VERCHK.include(r"lib\setup.py", match="version = ", splits=[('"',1)])
 VERCHK.include(r"lib\auxly\_modu.py", match="__version__ = ", splits=[('"',1)])
 VERCHK.include(r"CHANGELOG.adoc", match="auxly-", splits=[("-",1),(" ",0)], updatable=False)
+VERCHK.include(r"doc\source\conf.py", match="version = ", splits=[("'",1)])
+VERCHK.include(r"doc\source\conf.py", match="release = ", splits=[("'",1)])
 
 ##==============================================================#
 ## SECTION: Main Body                                           #
