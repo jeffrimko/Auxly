16b4c362114e727f4e4cd9279cc4af3e417228a4
jeffrimko <jeffrimko@gmail.com>
2020-02-17 21:51:12 -0500

    Updated walkfiles() to support older Python versions. Bumped version number.

--- lib/auxly/filesys.py
+++ lib/auxly/filesys.py
@@ -11,6 +11,7 @@ import os
 import os.path as op
 import re
 import shutil
+import sys
 from datetime import datetime
 
 ##==============================================================#
@@ -277,19 +278,30 @@ def delete(path, regex=None, recurse=False, test=False):
 def walkfiles(startdir, regex=None, recurse=True):
     """Yields the absolute paths of files found within the given start
     directory. Can optionally filter paths using a regex pattern."""
-    startdir = op.abspath(startdir)
-    with os.scandir(startdir) as it:
-        for i in it:
-            if i.is_file():
-                if regex:
-                    n = op.join(startdir, i.name)
-                    if _is_match(regex, n):
-                        yield n
-                else:
-                    yield op.join(startdir, i.name)
-            elif recurse:
-                for j in walkfiles(op.join(startdir, i.name), regex, recurse):
-                    yield j
+    if sys.version_info >= (3, 6):
+        startdir = op.abspath(startdir)
+        with os.scandir(startdir) as it:
+            for i in it:
+                if i.is_file():
+                    if regex:
+                        n = op.join(startdir, i.name)
+                        if _is_match(regex, n):
+                            yield n
+                    else:
+                        yield op.join(startdir, i.name)
+                elif recurse:
+                    for j in walkfiles(op.join(startdir, i.name), regex, recurse):
+                        yield j
+    else:
+        for r,_,fs in os.walk(startdir):
+            if not recurse and startdir != r:
+                return
+            for f in fs:
+                path = op.abspath(op.join(r,f))
+                if regex and not _is_match(regex, path):
+                    continue
+                if op.isfile(path):
+                    yield path
 
 def countfiles(path, recurse=False):
     """Returns the number of files under the given directory path."""
